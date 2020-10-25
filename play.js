a176d99a401047f38fee80e8c9b1c7f31a3abad4
joshua <wujoshua56@gmail.com>
2020-10-24 22:44:39 -0400

    yep

---
+++ play.js
@@ -0,0 +1,16 @@
+var name = 'Joshua';
+var age = 2;
+var hasHobbies = true;
+
+function summarizeUser(UserName, userAge, userHasHobby) {
+  return (
+    'Name is ' +
+    userName +
+    ', age is ' +
+    userAge +
+    ' and has hobbies ' +
+    userHasHobby
+  );
+}
+
+console.log(summarizeUser(name, age, hasHobbies));
